e75bdfc20ed6a63415b3c8cec9aa995b9fdcc162
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-05-17 00:12:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
+const boardRouter = require('./routes/boardRouter');
+
 const app = express();
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./serviceAccountKey.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL:
-    "https://hanbat-capstone-d4979-default-rtdb.asia-southeast1.firebasedatabase.app",
-});
-
-// 미들웨어 설정
+// 미들웨어 등록
 app.use(bodyParser.json());
 
-// 라우트 예시
-app.get("/", (req, res) => {
-  res.send("성공!");
-});
-
-// 게시판 라우트를 분리한 파일을 추가
-const boardRouter = require("./routes/boardRouter.js");
+// boardRouter를 Express 애플리케이션에 등록
 app.use("/board", boardRouter);
 
 // 서버 시작
