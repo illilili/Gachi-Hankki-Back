d4e1621fae485cf9a28b731ecc603f1af3eff3d6
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-05-21 21:42:41 +0900

    Delete route directory

--- route/signupRouter.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const SignupController = require('/Users/jo-eunhyeong/Gachi-Hankki-Back-test/controllers/SignupController');
-
-router.post('/signup', SignupController.signup);
-
-module.exports = router;
