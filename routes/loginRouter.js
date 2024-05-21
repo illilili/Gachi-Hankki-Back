f3643f84f91959dc21e94800c73f9ae0a2168b8f
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-05-21 14:36:28 +0900

    Update loginRouter.js

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const boardLoginController = require('./boardLoginController');
-const boardSignupController = require('./boardSignupController');
+const LoginController = require('./LoginController');
+const SignupController = require('./SignupController');
 
-router.post('/login', boardLoginController.login);
-router.post('/signup', boardSignupController.signup);
+router.post('/login', LoginController.login);
+router.post('/signup', SignupController.signup);
 
 module.exports = router;
