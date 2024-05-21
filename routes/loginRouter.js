0cf809b843c5586ce1e86eda164fd12679ec42f7
illilili <101322304+illilili@users.noreply.github.com>
2024-05-21 15:23:00 +0900

    Save local changes before pull

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const LoginController = require('./LoginController');
-const SignupController = require('./SignupController');
+const LoginController = require("../controller/LoginController");
+const SignupController = require("../controller/SignupController");
 
-router.post('/login', LoginController.login);
-router.post('/signup', SignupController.signup);
+router.post("/login", LoginController.login);
+router.post("/signup", SignupController.signup);
 
 module.exports = router;
