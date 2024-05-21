816ea2affe9ba2ac71680f084e3fdf3ea833b83f
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-05-21 15:09:46 +0900

    Update SignupController.js

--- controller/SignupController.js
+++ controller/SignupController.js
@@ -1,20 +1,17 @@
+const admin = require('firebase-admin');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const admin = require('firebase-admin');
-const { createUserWithEmailAndPassword } = require("firebase-admin/auth");
-const serviceAccount = require('./serviceAccountKey.json');
-const firebaseApp = admin.initializeApp(firebaseConfig);
+
 const auth = admin.auth();
 const db = admin.firestore();
 
-// 회원가입 핸들러
 exports.signup = async (req, res) => {
   try {
     const { hanbatEmail, password, confirmPassword, name, gender, department, birthdate, purpose, path } = req.body;
 
     // 필수 필드 유효성 검사
     if (!hanbatEmail || !password || !confirmPassword) {
-      return res.status(400).send('한밭대 이메일, 인증번호, 비밀번호, 비밀번호 확인은 필수 입력 사항입니다.');
+      return res.status(400).send('한밭대 이메일, 비밀번호, 비밀번호 확인은 필수 입력 사항입니다.');
     }
 
     // 비밀번호 확인
@@ -23,26 +20,29 @@ exports.signup = async (req, res) => {
     }
 
     // 비밀번호 해싱
-    const hashedPassword = await bcrypt.hash(password, 10); // 비밀번호를 해싱한 후에 저장
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     // Firebase Authentication을 사용하여 회원가입
-    const userCredential = await createUserWithEmailAndPassword(auth, hanbatEmail, password);
-    const user = userCredential.user;
+    const userRecord = await auth.createUser({
+      email: hanbatEmail,
+      password: hashedPassword
+    });
 
     // 회원가입 성공 시 토큰 발급
-    const token = jwt.sign({ userId: user.uid }, 'your_secret_key', { expiresIn: '1h' });
+    const token = jwt.sign({ userId: userRecord.uid }, 'your_secret_key', { expiresIn: '1h' });
 
     // 파이어베이스에 추가 정보 저장
-    await db.collection('users').doc(user.uid).set({
+    await db.collection('users').doc(userRecord.uid).set({
       name,
       gender,
       department,
       birthdate,
       purpose,
-      path
+      path,
+      passwordHash: hashedPassword // 비밀번호 해시 저장
     });
 
-    console.log('Additional information saved for user:', user.uid);
+    console.log('Additional information saved for user:', userRecord.uid);
     res.status(200).json({ token });
   } catch (error) {
     console.error('Error signing up:', error);
