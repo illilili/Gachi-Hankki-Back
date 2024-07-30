7a6ea6f21f83402ae2e5eb0583e3842ca2c04604
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-07-31 08:59:50 +0900

    Create validators.js

---
+++ utils/validators.js
@@ -0,0 +1,8 @@
+function validateHanbatEmail(email) {
+    const re = /^[a-zA-Z0-9._%+-]+@(edu\\.hanbat\\.ac\\.kr|hanbat\\.ac\\.kr)$/;
+    return re.test(String(email).toLowerCase());
+}
+
+module.exports = {
+    validateHanbatEmail
+};
