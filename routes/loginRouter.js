28fa5d9fe0a2eba78c70bde9393ee05a12ce91e1
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-08-29 15:36:26 +0900

    Update loginRouter.js

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -5,10 +5,10 @@ const LoginController = require('../controllers/LoginController');
 // 기존 로그인 라우트
 router.post('/login', LoginController.login);
 
-// 새로운 리프레시 토큰 라우트
+// 리프레시 토큰 라우트
 router.post('/refresh-token', LoginController.refreshToken);
 
-// 새로운 로그아웃 라우트
+//로그아웃 라우트
 router.post('/logout', LoginController.logout);
 
 module.exports = router;
