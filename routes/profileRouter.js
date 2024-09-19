6ab64fac35bd195dd5d34b4e3425db73ad8cc5b0
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-09-19 11:59:28 +0900

    Update profileRouter.js

--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,11 +1,19 @@
 const express = require('express');
-const { createProfile, getProfile } = require('../controllers/ProfileController');
+const { createProfile, getProfile, updateProfileImage, updateBio } = require('../controllers/ProfileController');
 const authenticateToken = require('../middlewares/authenticateToken'); 
 
 const router = express.Router();
 
+// 프로필 생성
 router.post('/', authenticateToken, createProfile);
 
+// 프로필 조회
 router.get('/', authenticateToken, getProfile);
+
+// 프로필 이미지 변경 
+router.patch('/image', authenticateToken, updateProfileImage);
+
+// 한줄 소개 변경 
+router.patch('/bio', authenticateToken, updateBio);
 
 module.exports = router;
