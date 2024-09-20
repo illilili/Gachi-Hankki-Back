38babdaee7de4e86d082ae532e28e48874873041
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-09-20 10:51:50 +0900

    Update chatRouter.js

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -1,17 +1,21 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const chatController = require("../controllers/chatController");
+const chatController = require('../controllers/chatController');
+const authenticateToken = require('../middlewares/authenticateToken');
 
-// POST 요청: 쪽지방 생성
-router.post("/rooms", chatController.createRoom);
+// POST 쪽지방 생성하기 (인증)
+router.post('/rooms', authenticateToken, chatController.createRoom);
 
-// POST 요청: 새로운 메시지 추가하기
-router.post("/rooms/:roomId/messages", chatController.addMessage);
+// GET 쪽지방 목록 가져오기 (인증)
+router.get('/rooms', authenticateToken, chatController.getRooms);
 
-// GET 요청: 특정 방의 메시지 가져오기
-router.get("/rooms/:roomId/messages", chatController.getMessages);
+// GET 특정 방의 메시지 가져오기 (인증)
+router.get('/rooms/:roomId/messages', authenticateToken, chatController.getMessages);
 
-// DELETE 요청: 쪽지방 삭제
-router.delete("/rooms/:roomId", chatController.deleteRoom);
+// POST 요청: 메시지 추가하기 
+router.post('/rooms/:roomId/messages', chatController.addMessage);
+
+// DELETE 요청: 쪽지방 삭제하기 
+router.delete('/rooms/:roomId', chatController.deleteRoom);
 
 module.exports = router;
