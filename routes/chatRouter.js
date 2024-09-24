8640e9a1d74e26f1907ecf8ea383b6f0b52c11c2
jeh0103 <166023112+jeh0103@users.noreply.github.com>
2024-09-24 22:30:42 +0900

    Update chatRouter.js

--- routes/chatRouter.js
+++ routes/chatRouter.js
@@ -6,14 +6,14 @@ const authenticateToken = require('../middlewares/authenticateToken');
 // POST 쪽지방 생성하기 (인증)
 router.post('/rooms', authenticateToken, chatController.createRoom);
 
-// GET 쪽지방 목록 가져오기 (인증)
-router.get('/rooms', authenticateToken, chatController.getRooms);
-
-// GET 특정 방의 메시지 가져오기 (인증)
-router.get('/rooms/:roomId/messages', authenticateToken, chatController.getMessages);
-
 // POST 요청: 메시지 추가하기 
 router.post('/rooms/:roomId/messages', chatController.addMessage);
+
+// GET 사용자의 쪽지방 목록 가져오기 (인증)
+router.get('/rooms', authenticateToken, chatController.getRooms);
+
+// GET 특정 방의 메시지 전체 가져오기
+router.get('/rooms/:roomId/messages', chatController.getMessages); 
 
 // DELETE 요청: 쪽지방 삭제하기 
 router.delete('/rooms/:roomId', chatController.deleteRoom);
